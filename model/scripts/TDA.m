9729d4532892e6ede71fc2b82d088a85e0db152e
Aashiq Dheeraj <ad163@teer37.oit.duke.edu>
2013-12-09 19:19:38 -0500

    tda script

---
+++ model/scripts/TDA.m
@@ -0,0 +1,7 @@
+javaclasspath('../RCA1Portable/jars/tda.jar');
+import api.*
+tda = Tda()
+X = sample_data('swiss',300,3,0);
+tda.RCA1( { 'settingsFile=../RCA1Portable/data/cts.txt', 'supplyDataAs=pointCloud','distanceBoundOnEdges=20'}, X );
+I = tda.getResultsRCA1(0).getIntervals();
+J = tda.getResultsRCA1(1).getIntervals();
--- model/scripts/prediction/kern.r
+++ model/scripts/prediction/kern.r
@@ -2,27 +2,6 @@ require("kernlab")
 train <- data.frame(read.csv("data/num/separate_ages/train.csv", header=TRUE))
 test <- data.frame(read.csv("data/num/separate_ages/test.csv", header = TRUE))
 
-noty <- c("name", "sex", "embarked.C",
-	 "embarked.Q", "embarked.S","age", "fare",
-	 "pclass", "sibsp", "parch")
-all <- c("survived", noty)
-binvars <- c("name", "sex", "embarked.C",
- "embarked.Q", "embarked.S")
-convars <- c("age", "fare")
-ordvars <- c("pclass", "sibsp", "parch")
-
-# "survived","pclass","name","sex","age","sibsp","parch","fare","embarked.C","embarked.Q","embarked.S"
-
-assignTypes <- function(data){
-	data[,noty] <- sapply(data[,noty], as.numeric)
-	if("survived" %in% colnames(data)){
-		data[,"survived"] <- as.factor(data[,"survived"])	
-	}
-	return(data)
-}
-
-train <- assignTypes(train)
-test <- assignTypes(test)
 
 ntrain <- scale(as.matrix(sapply(train[,noty], as.numeric), center = TRUE))
 ntest <- scale(as.matrix(sapply(test[,noty], as.numeric), center = TRUE))
@@ -40,10 +19,6 @@ pctest <- ntest %*% treig$vectors[,1:vn]
 # dev.off()
 # 	diag(treig$values[1:2]) %*% t(treig$vectors[,1:2])
 
-# k <- ksvm(x = as.matrix(train[,noty]), y = as.factor(train[,"survived"]), scaled = TRUE, 
-# 	kernel = "rbfdot", type = "C-svc", kpar = "automatic"
-# ,na.action = na.omit)
-
 # # kpca <- ksvm(x = pctrain, y = as.factor(train[,"survived"]), scaled = TRUE, 
 # # 	kernel = "rbfdot", type = "C-svc", kpar = "automatic"
 # # ,na.action = na.omit)
@@ -51,7 +26,3 @@ pctest <- ntest %*% treig$vectors[,1:vn]
 # # pred_pca <- predict(object = kpca, newdata = pctest, type = "response")
 # pred <- predict(object = k, newdata = as.matrix(test[,noty]), type = "response")
 
-# pids <- nrow(train) + as.numeric(row.names(test))
-# subm <- cbind(pids, as.numeric(pred)-1)
-# colnames(subm) <- c("PassengerId", "Survived")
-# write.table(subm, file="predictions/svm_rbf.csv", row.names=FALSE, col.names=TRUE, sep=",")
