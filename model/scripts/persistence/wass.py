e23599106f7d3012777743b977d12133be58d80f
Christian Drappi <christiandrappi@gmail.com>
2013-12-11 19:32:19 -0500

    added algo to compute wasserstein

--- model/scripts/persistence/wass.py
+++ model/scripts/persistence/wass.py
@@ -1,2 +1,32 @@
+import csv
+from os import listdir
+from os.path import isfile, join
 from dionysus import *
-dia = PersistenceDiagram(1)
+
+def csv_to_list(filename):
+    ret = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            ret.append(tuple(row))
+    return ret
+
+def dist(a, b, p):
+    return wasserstein_distance(a,b,p)
+
+num_chunks = 30
+p = 2
+data_directory = "/Users/christiandrappi/Dropbox/top_proj/small_diagrams/" + str(num_chunks) + "/"
+
+files = [data_directory+f for f in listdir(data_directory) if isfile(join(data_directory,f)) and f.endswith(".csv") ]
+files.sort()
+
+n = len(files)
+wmat = [[0] * n ] * n
+for e1, f1 in enumerate(files):
+    for e2, f2 in enumerate(files):
+        diag1 = PersistenceDiagram(1,csv_to_list(f1))
+        diag2 = PersistenceDiagram(1,csv_to_list(f2))
+        wmat[e1][e2] = dist(diag1,diag2,p)
+
+# print(wmat)
