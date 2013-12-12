41b536c4ed33399a6a38bae58e266753a7873ac6
wisefool769 <aashiq.d@gmail.com>
2013-12-12 02:51:37 -0500

    moregraphs

--- perhom.r
+++ perhom.r
@@ -3,83 +3,62 @@ library(datasets)
 library(phom)
 library(ggplot2)
  
-# x <- runif(100)
-# y <- runif(100)
-# points <- t(as.matrix(rbind(x, y)))
- 
-# max_dim <- 1
-# max_f <- 0.2
- 
-# intervals <- pHom(points, max_dim, max_f, metric="manhattan")
- 
-# plotPersistenceDiagram(intervals, max_dim, max_f, 
-#     title="Random Points in Cube with l_1 Norm")
- 
- 
-### Example 2
 
-# t <- 2 * pi * runif(100)
-# x <- cos(t)
-# y <- sin(t)
-# X <- t(as.matrix(rbind(x, y)))
  
-# max_dim <- 2
-# max_f <- 0.6
- 
-# intervals <- pHom(X, max_dim, max_f)
- 
-# plotPersistenceDiagram(intervals, max_dim, max_f,
-#     title="Random Points on S^1 with Euclidean Norm")
+
 
 data(EuStockMarkets)
 eu <- unique(EuStockMarkets)
-chunks.size <- 90
-num.chunks <- as.integer(nrow(eu)/chunks.size)
+l <- dim(eu)[1]
+lr <- log(eu[2:l,]/eu[1:l-1,])
+somelr200 <- lr[sample(nrow(lr),size=200,replace=TRUE),]
+somelr100 <- lr[sample(nrow(lr),size=100,replace=TRUE),]
+somelr500 <- lr[sample(nrow(lr),size=500,replace=TRUE),]
+write.table(somelr200, file = "model/data/lr_sample-200.csv", sep = ",")
+write.table(somelr100, file = "model/data/lr_sample-100.csv", sep = ",")
+write.table(somelr500, file = "model/data/lr_sample-500.csv", sep = ",")
 
-for (i in 1:num.chunks) {
-    eu <- unique(EuStockMarkets)
-    eu.filter <- seq(from=1, to=chunks.size, by=1) + (i-1)*chunks.size
 
-    eu <- eu[eu.filter,]
-    l <- nrow(eu)
-
-    X <- log(eu[2:l,]/eu[1:l-1,])
-
-    max_dim <- 1
-    max_f <- 1
-
-    intervals <- pHom(X, max_dim, max_f)
-    # diagram.0 <- data.frame(intervals[intervals[,1] == 0,])
-    # colnames(diagram.0) <- c("dim", "birth", "death")
-
-    diagram.1 <- data.frame(intervals[intervals[,1] == 1,])
-    diagram.1 <- diagram.1[,-1]
-    colnames(diagram.1) <- c("birth", "death")
-
-    title.csv <- paste("small_diagrams/diagram_", chunks.size, "_", i, ".csv", sep="")
-    write.table(diagram.1, file=title.csv, row.names=FALSE, col.names=FALSE, sep=",")
-}
-
-# intervals <- pHom(X, max_dim, max_f)
-
-# diagram.0 <- data.frame(intervals[intervals[,1] == 0,])
-# colnames(diagram.0) <- c("dim", "birth", "death")
-
-# diagram.1 <- data.frame(intervals[intervals[,1] == 1,])
-# colnames(diagram.1) <- c("dim", "birth", "death")
-
-# diagram.2 <- data.frame(intervals[intervals[,1] == 2,])
-# colnames(diagram.2) <- c("dim", "birth", "death")
-
-# plotPersistenceDiagram(intervals, max_dim, max_f, title="EuStockMarkets")
-
-# p <- qplot(birth, death, data = diagram.1) + geom_point(colour="red") 
-# p1 <- p + ggtitle("1-dimensional persistence diagram") + geom_abline(intercept=0, slope=1)
+# ns <- 100000
+# lrs <- sapply(1:n, function(x) sqrt(sum((lr[sample(1833,1),] - lr[sample(1833,1),])^2)))
+# dat <- data.frame(lrs)
+#colnames(dat) <- c("dist")
+# q <- ggplot(dat, aes(x = dist, y =..density..)) + geom_bar(binwidth = 0.001) + xlab("Euclidean Distance") + ylab("Density") + ggtitle("Log-Return Distance Distribution")
 
 # setEPS()
-# postscript("1d_diagram.eps")
-# plot(p1)
+# postscript("writeup/lr-dist2.eps")
+# plot(q)
 # dev.off()
+
+
+# max_dim <- 1
+# max_f <- 0.1
+
+# intervals <- pHom(somelr, max_dim, max_f)
+
+# diagram <- data.frame(intervals[intervals[,1] == 1,])
+# colnames(diagram) <- c("dim", "birth", "death")
+
+
+
+# # # for(nc in c("30", "60", "90", "100")){
+# # #     for(np in c("0", "1")){
+# # #         f <- paste("p2-",np,"-",nc, sep = "")
+# # #         diagram <- data.frame(read.csv(paste("model/data/psquared/",f,".csv",sep = ""), header = FALSE))
+# #         # colnames(diagram) <- c("birth", "death")
+        # p <- qplot(birth, death, data = diagram) + geom_point(colour="red") 
+        # p1 <- p + ggtitle(paste("0-dimensional persistence diagram", sep = "")) + geom_abline(intercept=0, slope=1)
+
+        # setEPS()
+        # postscript(paste("writeup/rlr-200-0-f1e-2.eps",sep = ""))
+        # plot(p1)
+        # dev.off()
+# #     }
+
+# # }
+
+
+
 
 
 
