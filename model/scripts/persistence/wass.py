4c8b6fb667ba0c3ec520e63bbc5c70eee976d144
wisefool769 <aashiq.d@gmail.com>
2013-12-11 19:49:05 -0500

    wass kernels

--- model/scripts/persistence/wass.py
+++ model/scripts/persistence/wass.py
@@ -8,18 +8,20 @@ def csv_to_list(filename):
     with open(filename, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
-            ret.append(tuple(row))
+            ret.append(tuple([float(i) for i in row]))
     return ret
 
 def dist(a, b, p):
     return wasserstein_distance(a,b,p)
 
-num_chunks = 30
-p = 2
-data_directory = "/Users/christiandrappi/Dropbox/top_proj/small_diagrams/" + str(num_chunks) + "/"
+num_chunks = 90
+p = 1
+data_directory = "/Users/aashiq/Dropbox/top_proj/small_diagrams/" + str(num_chunks) + "/"
 
 files = [data_directory+f for f in listdir(data_directory) if isfile(join(data_directory,f)) and f.endswith(".csv") ]
 files.sort()
+
+print(csv_to_list(files[1]))
 
 n = len(files)
 wmat = [[0] * n ] * n
@@ -28,5 +30,15 @@ for e1, f1 in enumerate(files):
         diag1 = PersistenceDiagram(1,csv_to_list(f1))
         diag2 = PersistenceDiagram(1,csv_to_list(f2))
         wmat[e1][e2] = dist(diag1,diag2,p)
+
+
+with open("wass_kern_" + str(num_chunks) + ".csv", 'w') as f:
+    writer = csv.writer(f, delimiter = ',')
+    for r in wmat:
+        writer.writerow(r)
+
+
+
+print(wmat)
 
 # print(wmat)
